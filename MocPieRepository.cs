817ff62125b3042b75fe73b3b42cae6a09a551d9
ismailkoken <39409440+ismailkoken@users.noreply.github.com>
2020-02-04 17:03:49 +0300

    tw

---
+++ MocPieRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MocPieRepository : IPieRepository
+    {
+        public IEnumerable<Pie> GetAllPies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
